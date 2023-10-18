e12ed16c90c811541dbc44f7a895662cb8c989b6
pppbigbud <pppbigbud@gmail.com>
2023-10-18 14:54:25 +0200

    testdeploiementV1

---
+++ boutiqueCustom.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+  Template Name: Boutique Custom
+
+  Template Name: Full-width
+
+  Template Post Type: post, page, product
+
+*/
+get_header();
+
+?>
+
+<div class="test"></div>
+[products limit="12" columns="4"]
+
+<?php
+get_footer(); ?>
